a9885b89d62dfd9e5c97300444e1e9433e8951d9
metzgermeister <metzgermesiter87@gmail.com>
2013-09-21 17:53:49 +0300

    more calculations

--- src/org/oracul/Oracul.java
+++ src/org/oracul/Oracul.java
@@ -31,19 +31,19 @@ public class Oracul {
             // Calc u
             setFU(data.U, data.V, data.H, data.F1, data.F2);
             System.arraycopy(data.U, 0, data.C, 0, SZ_LAM * SZ_FI);
-            // SplitLam(data.U, data.C, data.Mu1, data.F1, data.U1);
+            splitLam(data.U, data.C, data.Mu1, data.F1, data.U1);
 
             System.arraycopy(data.V, 0, data.C, 0, SZ_LAM * SZ_FI);
-            // SplitFi(data.U, data.C, data.Mu2, data.F2, data.U2);
+            splitFi(data.U, data.C, data.Mu2, data.F2, data.U2);
 
             // Calc v
             // SetFV(data.U, data.V, data.H, data.F1, data.F2);
 
             System.arraycopy(data.U, 0, data.C, 0, SZ_LAM * SZ_FI);
-            // SplitLam(data.V, data.C, data.Mu1, data.F1, data.V1);
+            splitLam(data.V, data.C, data.Mu1, data.F1, data.V1);
 
             System.arraycopy(data.V, 0, data.C, 0, SZ_LAM * SZ_FI);
-            // SplitFi(data.V, data.C, data.Mu2, data.F2, data.V2);
+            splitFi(data.V, data.C, data.Mu2, data.F2, data.V2);
 
             // Final
             // SplitSum(data.U, data.U1, data.U2);
@@ -116,4 +116,141 @@ public class Oracul {
             HH[i] = quadrInterpol(r, data.H1[i], data.H2[i], data.H3[i]);
 
     }
+
+
+    private void splitLam(double[] R, double[] CC, double[] MMu, double[] FF1, double[] R1) {
+        boolean flg1, flg2;
+        int i, j, beg = 0;
+        double p, q, pm, qm, g, z, h;
+
+        for (j = 1; j <= (SZ_FI - 2); j++) {
+            flg1 = true;
+            z = dt / (2 * dh * dh * ERH_RDS_2 * cos(j * dh) * cos(j * dh));
+            h = dh * ERH_RDS * cos(j * dh);
+            for (i = 1; i <= (SZ_LAM - 2); i++) {
+                if (CC[j * SZ_LAM + i] > 0.) {
+                    flg2 = true;
+                } else if (CC[j * SZ_LAM + i] == 0.) {
+                    flg2 = flg1;
+                } else {
+                    flg2 = false;
+                }
+
+                if (flg2) {
+                    p = z * (h * CC[j * SZ_LAM + i] + MMu[j * SZ_LAM + i] + MMu[j * SZ_LAM + i - 1]);
+                    q = z * (h * CC[j * SZ_LAM + i] - MMu[j * SZ_LAM + i] - MMu[j * SZ_LAM + i + 1]);
+
+                    if (!flg1) {// from "-" (i-1) to "+" (i)
+                        pm = z * (-h * CC[j * SZ_LAM + i - 1] + MMu[j * SZ_LAM + i - 1] + MMu[j * SZ_LAM + i]);
+                        qm = z * (h * CC[j * SZ_LAM + i - 1] + MMu[j * SZ_LAM + i - 1] + MMu[j * SZ_LAM + i - 2]);
+
+                        g = 1. + p + pm;
+                        q = R[j * SZ_LAM + i] + FF1[j * SZ_LAM + i] + q * (R[j * SZ_LAM + i] - R[j * SZ_LAM + i + 1]);
+                        qm = R[j * SZ_LAM + i - 1] + FF1[j * SZ_LAM + i - 1] + qm * (R[j * SZ_LAM + i - 2] - R[j * SZ_LAM + i - 1]);
+                        p = pm * q + p * qm;
+
+                        R1[j * SZ_LAM + i - 1] = (qm + p) / g;
+                        R1[j * SZ_LAM + i] = (q + p) / g;
+
+                        minusX1(beg, i - 2, j, R, CC, MMu, FF1, R1);
+                    } else {// "+", "+" (pCC[i]>0)
+                        R1[j * SZ_LAM + i] = R1[j * SZ_LAM + i - 1] + (R[j * SZ_LAM + i] - R1[j * SZ_LAM + i - 1]
+                                + q * (R[j * SZ_LAM + i] - R[j * SZ_LAM + i + 1]) + FF1[j * SZ_LAM + i]) / (1. + p);
+                    }
+                } else if (flg1) {
+                    beg = i;// from "+" (i-1) to "-" (i)
+                }
+
+                flg1 = flg2;
+            }
+
+            if (!flg1) {
+                minusX1(beg, SZ_LAM - 2, j, R, CC, MMu, FF1, R1);
+            }
+
+        }
+    }
+
+
+    void minusX1(int i1, int i2, int j, double[] R, double[] CC,
+                 double[] MMu, double[] FF1, double[] R1) {
+        double p, q, z = dt / (2. * dh * dh * ERH_RDS_2 * cos(j * dh) * cos(j * dh)),
+                h = dh * ERH_RDS * cos(j * dh);
+
+        for (int i = i2; i >= i1; i--) {
+            p = z * (-h * CC[j * SZ_LAM + i] + MMu[j * SZ_LAM + i] + MMu[j * SZ_LAM + i + 1]);
+            q = z * (h * CC[j * SZ_LAM + i] + MMu[j * SZ_LAM + i] + MMu[j * SZ_LAM + i - 1]);
+
+            R1[j * SZ_LAM + i] = R1[j * SZ_LAM + i + 1] + (R[j * SZ_LAM + i] - R1[j * SZ_LAM + i + 1]
+                    + q * (R[j * SZ_LAM + i - 1] - R[j * SZ_LAM + i]) + FF1[j * SZ_LAM + i]) / (1. + p);
+        }
+    }
+
+
+    private void splitFi(double[] R, double[] CC, double[] MMu, double[] FF2, double[] R2) {
+        boolean flg1, flg2;
+        int i, j, beg = 0;
+        double p, q, pm, qm, g, z = dt / (2 * dh * dh * ERH_RDS_2),
+                h = dh * ERH_RDS;
+
+        for (i = 1; i <= (SZ_LAM - 2); i++) {
+            flg1 = true;
+            for (j = 1; j <= (SZ_FI - 2); j++) {
+                if (CC[j * SZ_LAM + i] > 0.) {
+                    flg2 = true;
+                } else if (CC[j * SZ_LAM + i] == 0.) flg2 = flg1;
+                else {
+                    flg2 = false;
+                }
+
+                if (flg2) {
+                    p = z * (h * CC[j * SZ_LAM + i] + MMu[j * SZ_LAM + i] + MMu[(j - 1) * SZ_LAM + i]);
+                    q = z * (h * CC[j * SZ_LAM + i] - MMu[j * SZ_LAM + i] - MMu[(j + 1) * SZ_LAM + i]);
+
+                    if (!flg1) {// from "-" (j-1) to "+" (j)
+                        pm = z * (-h * CC[(j - 1) * SZ_LAM + i] + MMu[(j - 1) * SZ_LAM + i] + MMu[j * SZ_LAM + i]);
+                        qm = z * (h * CC[(j - 1) * SZ_LAM + i] + MMu[(j - 1) * SZ_LAM + i] + MMu[(j - 2) * SZ_LAM + i]);
+
+                        g = 1. + p + pm;
+                        q = R[j * SZ_LAM + i] + FF2[j * SZ_LAM + i] + q * (R[j * SZ_LAM + i] - R[(j + 1) * SZ_LAM + i]);
+                        qm = R[(j - 1) * SZ_LAM + i] + FF2[(j - 1) * SZ_LAM + i] + qm * (R[(j - 2) * SZ_LAM + i] - R[(j - 1) * SZ_LAM + i]);
+                        p = pm * q + p * qm;
+
+                        R2[(j - 1) * SZ_LAM + i] = (qm + p) / g;
+                        R2[j * SZ_LAM + i] = (q + p) / g;
+
+                        minusX2(beg, j - 2, i, R, CC, MMu, FF2, R2);
+                    } else // "+", "+" (pCC[j]>0)
+                    {
+                        R2[j * SZ_LAM + i] = R2[(j - 1) * SZ_LAM + i] + (R[j * SZ_LAM + i] - R2[(j - 1) * SZ_LAM + i]
+                                + q * (R[j * SZ_LAM + i] - R[(j + 1) * SZ_LAM + i]) + FF2[j * SZ_LAM + i]) / (1. + p);
+                    }
+                } else if (flg1) beg = j; // from "+" (j-1) to "-" (j)
+
+                flg1 = flg2;
+            }
+
+            if (!flg1) {
+                minusX2(beg, SZ_FI - 2, i, R, CC, MMu, FF2, R2);
+            }
+        }
+    }
+
+
+    private void minusX2(int j1, int j2, int i, double[] R, double[] CC,
+                         double[] MMu, double[] FF2, double[] R2) {
+        int j;
+        double p, q, z = dt / (2. * dh * dh * ERH_RDS_2),
+                h = dh * ERH_RDS;
+
+        for (j = j2; j >= j1; j--) {
+            p = z * (-h * CC[j * SZ_LAM + i] + MMu[j * SZ_LAM + i] + MMu[(j + 1) * SZ_LAM + i]);
+            q = z * (h * CC[j * SZ_LAM + i] + MMu[j * SZ_LAM + i] + MMu[(j - 1) * SZ_LAM + i]);
+
+            R2[j * SZ_LAM + i] = R2[(j + 1) * SZ_LAM + i] + (R[j * SZ_LAM + i] - R2[(j + 1) * SZ_LAM + i]
+                    + q * (R[(j - 1) * SZ_LAM + i] - R[j * SZ_LAM + i]) + FF2[j * SZ_LAM + i]) / (1. + p);
+        }
+    }
+
+
 }
